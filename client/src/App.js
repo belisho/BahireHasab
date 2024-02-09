2f2e884595f3b8113a9102fbcb334d9403c93add
belisho <belete.a1627@gmail.com>
2024-02-09 14:09:09 +0300

    add home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import { useState } from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './pages/Home.jsx'
 function App() {
   return (
-    <div className="App">
-      <h1>Bahire Hasab project</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
