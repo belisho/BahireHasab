35bef979a26596ba78126cec0c28e3409451ebd8
belisho <belete.a1627@gmail.com>
2024-02-22 15:46:17 +0300

    using commponent state on each button component

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import {React} from "react"
+// import {useState} from "react";
 
-function Home(){
+import { useState } from "react";
+
+export default function Home(){
     return(
-        <div className="Home">
-            <h1>ባሕረ ሐሳብ</h1>
+        <div>
+            <h1>ባህረ ሐሳብ</h1>
+            <ButtonComponent />
+            <ButtonComponent />
         </div>
-    )
+    );
 }
+function ButtonComponent(){
+const [count, setCount] = useState(0);
 
-export default Home;
+    function eventHandler(){
+        setCount(count + 1);
+    }
+return(
+<div>
+    <button onClick={eventHandler}>Click {count} times</button>
+</div>
+);
+}
